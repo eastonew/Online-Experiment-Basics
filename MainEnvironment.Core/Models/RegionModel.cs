cc6e07d31a9c8aa6bd34b3227ba81ec70318cd39
Edward Easton <eastonew@aston.ac.uk>
2021-07-30 13:30:49 +0100

    Update Region Model to have all required properties

--- MainEnvironment.Core/Models/RegionModel.cs
+++ MainEnvironment.Core/Models/RegionModel.cs
@@ -6,6 +6,11 @@ namespace MainEnvironment.Core
 {
     public class RegionModel
     {
+        public Guid Identifier { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
         public string RegionName { get; set; }
         public int Order { get; set; }
     }
