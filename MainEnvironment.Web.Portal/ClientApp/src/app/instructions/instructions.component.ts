8ec73bccc37afc5565c101c033aa7ac9e97c8315
Edward Easton <eastonew@aston.ac.uk>
2021-08-11 17:41:06 +0100

    Fixed some registration issues with components

--- MainEnvironment.Web.Portal/ClientApp/src/app/instructions/instructions.component.ts
+++ MainEnvironment.Web.Portal/ClientApp/src/app/instructions/instructions.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'oeb-register',
-  templateUrl: './register.component.html',
-  styleUrls: ["./register.component.css"]
+  selector: 'ore-instructions',
+  templateUrl: './instructions.component.html',
+  styleUrls: ["./instructions.component.css"]
 })
-export class RegisterComponent {
+export class InstructionsComponent {
 
   public IntroText: string = "Welcome, this portal will allow you to consent to the Measuring Symmetry Preference by Age experiment and recieve the instructions to download the Virtual Reality Environment.";
 
